aab0f56a3b1eb66c24357c39f4b72521e69f2bcc
BenKnight <benny@MacBook-Air.local>
2023-11-02 14:54:44 +0000

    added updated version of bad code with extra line

--- bad.R
+++ bad.R
@@ -3,6 +3,7 @@ fun = function(one)
   one.plus.one <- oen + 1
   four <- newVar <- matrix(1:10,nrow = 2)
   four[ 1, ]
+  print("this is a change")
   txt <- 'hi'
   three <- two+ 1
   if(txt == 'hi') 4
